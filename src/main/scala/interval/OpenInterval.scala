7b326db46489c99ecd57ca93bfa6ab01ad8ba287
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2013-10-14 03:43:42 -0700

    implementation of OpenInterval#equals().

--- src/main/scala/interval/OpenInterval.scala
+++ src/main/scala/interval/OpenInterval.scala
@@ -10,6 +10,12 @@ class OpenInterval(val lowerPoint: Int, val upperPoint: Int) {
   def contains(p: Int): Boolean = 
     (lowerPoint < p && p < upperPoint)
 
+  override def equals(other:Any) = other match {
+    case that: OpenInterval =>
+     lowerPoint == that.lowerPoint && upperPoint == that.upperPoint
+    case _ => false
+  }
+
   override def toString: String =
     "[%d,%d]".format(lowerPoint, upperPoint)
 } 
