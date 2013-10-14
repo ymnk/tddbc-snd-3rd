b1e79ffc2f93a2e3c37718c85d07f5da31b2e645
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2013-10-13 17:08:25 -0700

    refactoring tests

--- src/test/scala/interval/ClosedIntervalSpec.scala
+++ src/test/scala/interval/ClosedIntervalSpec.scala
@@ -7,9 +7,12 @@ import java.io.{ByteArrayOutputStream => BAOS, ByteArrayInputStream => BAIS}
 import java.io.{ObjectOutputStream => OOS, ObjectInputStream => OIS}
 import java.io._
 
-class Subject_1_1 extends FlatSpec with BeforeAndAfter with ShouldMatchers {
+class ClosedIntervalSpec extends FlatSpec with BeforeAndAfter with ShouldMatchers {
+
+  var interval: ClosedInterval = _
 
   before {
+    interval = new ClosedInterval(3, 8) 
   }
 
   after {
@@ -21,28 +24,24 @@ class Subject_1_1 extends FlatSpec with BeforeAndAfter with ShouldMatchers {
   }
 
   it should "impelement the pretty printer." in {
-    val interval = new ClosedInterval(3, 8) 
     interval.toString should equal ("[3,8]")
   }
 
   it should "throw an exception if its arguments are wrong." in {
     intercept[IntervalException] {
-      val interval = new ClosedInterval(8, 3) 
+      new ClosedInterval(8, 3) 
     }
   }
 
   it can "return its lower point." in {
-    val interval = new ClosedInterval(3, 8) 
     interval.lowerPoint should equal (3)
   }
 
   it can "return its upper point." in {
-    val interval = new ClosedInterval(3, 8) 
     interval.upperPoint should equal (8)
   }
 
   it can "contains method." in {
-    val interval = new ClosedInterval(3, 8) 
     interval.contains(3) should equal (true)
     interval.contains(-1) should equal (false)
   }
