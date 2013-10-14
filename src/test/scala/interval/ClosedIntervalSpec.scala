0527fc6df96644348b1d8c533992dbfb4b52e0b4
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2013-10-13 17:02:58 -0700

    ClosedInterval supports "contains" method.

--- src/test/scala/interval/ClosedIntervalSpec.scala
+++ src/test/scala/interval/ClosedIntervalSpec.scala
@@ -46,46 +46,4 @@ class Subject_1_1 extends FlatSpec with BeforeAndAfter with ShouldMatchers {
     interval.contains(3) should equal (true)
     interval.contains(-1) should equal (false)
   }
-
-/*
-  behavior of "ZOutputStream and ZInputStream"
-
-  it can "deflate and inflate data." in {
-    val hello = "Hello World!"
-
-    val out = new BAOS()
-    val zOut = new ZOutputStream(out, Z_BEST_COMPRESSION)
-    val objOut = new OOS(zOut)
-    objOut.writeObject(hello)
-    zOut.close
-
-    val in = new BAIS(out.toByteArray())
-    val zIn = new ZInputStream(in)
-    val objIn = new OIS(zIn)
-
-    objIn.readObject.toString should equal (hello)
-  }
-
-  behavior of "ZOutputStream and ZInputStream"
-
-  it can "support nowrap data." in {
-
-    implicit val buf = new Array[Byte](100)
-
-    val hello = "Hello World!".getBytes
-
-    val baos = new BAOS
-    val zos = new ZOutputStream(baos, Z_DEFAULT_COMPRESSION, true)
-    hello -> zos
-    zos.close
-
-    val baos2 = new BAOS
-    val zis = new ZInputStream(new BAIS(baos.toByteArray), true)
-    zis -> baos2
-    val data2 = baos2.toByteArray
-
-    data2.length should equal (hello.length)
-    data2 should equal (hello)
-  }
-  */ 
 }
