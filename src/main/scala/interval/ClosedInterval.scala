33ece0299da2e3568fb963e18bfe16ea081b1e55
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2013-10-15 16:41:21 -0700

    implementations of infinite intervals.

--- src/main/scala/interval/ClosedInterval.scala
+++ src/main/scala/interval/ClosedInterval.scala
@@ -2,12 +2,10 @@ package interval
 
 object ClosedInterval {
   val mark = ("[", "]")
-  def parse(str: String): ClosedInterval = Interval.parse(str, mark) match {
-    case (x, y) => 
-      ClosedInterval(Integer.parseInt(x), Integer.parseInt(y))
-  }
+  def parse(str: String): Interval =
+     Interval.parse(str, mark, ClosedInterval.apply _)
 }
 
-case class ClosedInterval(val lowerPoint: Int, val upperPoint: Int)
+case class ClosedInterval(val lowerPoint: Point, val upperPoint: Point)
   extends Interval(ClosedInterval.mark._1, ClosedInterval.mark._2) {
 } 
--- src/main/scala/interval/OpenClosedInterval.scala
+++ src/main/scala/interval/OpenClosedInterval.scala
@@ -2,13 +2,10 @@ package interval
 
 object OpenClosedInterval {
   val mark = ("(", "]")
-  def parse(str: String): OpenClosedInterval =
-    Interval.parse(str, mark) match {
-      case (x, y) =>
-        OpenClosedInterval(Integer.parseInt(x), Integer.parseInt(y))
-    }
+  def parse(str: String): Interval =
+    Interval.parse(str, mark, OpenClosedInterval.apply _)
 }
 
-case class OpenClosedInterval(val lowerPoint: Int, val upperPoint: Int)
+case class OpenClosedInterval(val lowerPoint: Point, val upperPoint: Point)
   extends Interval(OpenClosedInterval.mark._1, OpenClosedInterval.mark._2) {
 } 
